4295c7f875d68650652311474c9dfdca9c1b7d51
Justin <justinagui988@gmail.com>
2025-09-17 12:28:23 +0300

    chore: assigned routes complete

---
+++ server/app/api/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from app.extensions import db
+from app.api.orders import orders_bp
+from app.api.admin_reset_password import admin_reset_bp
+from app.api.categories import categories_bp
+
+def create_app():
+    app = Flask(__name__)
+    db.init_app(app)
+
+    app.register_blueprint(orders_bp)
+    app.register_blueprint(admin_reset_bp)
+    app.register_blueprint(categories_bp)
+
+    return app
